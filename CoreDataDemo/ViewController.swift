e29116a4f8665f84e18fd53c9f3d098b56e85e4f
Nikolay <halemhood@icloud.com>
2022-03-20 12:41:56 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Николай Петров on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBacColor()
+    }
+    
+    // MARK: - Private Methods
+    private func setBacColor(){
+        view.backgroundColor = .white
+    }
+}
+
