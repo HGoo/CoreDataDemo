22e0d6bcaa050bf81ef604397f9d180272477958
Nikolay <halemhood@icloud.com>
2022-03-22 15:15:39 +0300

    Add delete and edit action

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
     private let cellID = "cell"
     private var tasks: [Task] = []
     private let manageContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var isEditingMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -94,7 +95,7 @@ class ViewController: UITableViewController {
     }
     
     private func save(_ taskName: String) {
-
+        
         // Entity name
         guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: manageContext) else { return }
         
@@ -126,6 +127,61 @@ class ViewController: UITableViewController {
             print(error.localizedDescription)
         }
     }
+    
+    private func delete(rowIndexPathAt  indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, _) in
+            guard let self = self else { return }
+            
+            
+            self.tasks.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+        return action
+    }
+    
+    private func edit(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "Edit") { _, _, _ in
+            
+            let alert = UIAlertController(
+                title: "Do you want to edit?",
+                message: "You could do so when we implemebt this functionality",
+                preferredStyle: .alert
+            )
+            
+            let edit = UIAlertAction(title: "OK", style: .default, handler: { _ in
+                guard let task = alert.textFields?.first?.text, !task.isEmpty else {
+                    print("The text field is empty")
+                    return
+                }
+                self.tasks[indexPath.row].name = task
+                self.tableView.reloadData()
+            })
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                self.tableView.reloadData()
+            }
+            
+            alert.addAction(edit)
+            alert.addAction(cancel)
+            alert.addTextField { text in
+                text.text = self.tasks[indexPath.row].name
+            }
+            
+            self.present(alert, animated: true)
+            
+        }
+        return action
+    }
+    
+    // MARK: - Table View Data Sourse
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let edit = self.edit(rowIndexPathAt: indexPath)
+        let delete = self.delete(rowIndexPathAt: indexPath)
+        let swipe = UISwipeActionsConfiguration(actions: [delete, edit])
+        
+        return swipe
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -146,7 +202,6 @@ extension ViewController {
         let task = tasks[indexPath.row]
         
         content.text = task.name
-        
         
         cell.contentConfiguration = content
         
